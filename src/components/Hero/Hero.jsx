c462946ce08c273f181bb5c9895f2d87b2aab98a
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-10 18:27:42 +0600

    experience upated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ function Hero() {
       <img className='profile-image' src={profileImage} alt="Profile Picture" />
 
       <h1> <span>I&apos;m Shafiq Khan,</span> Fullstack Developer Based in Bangladesh</h1>
-      <p>I am a Fullstack Developer with 5 years of experience specializing in the MERN stack and Nextjs, and I focus on building dynamic, scalable web applications</p>
+      <p>I am a Fullstack Developer with 2 years of experience specializing in the MERN stack and Nextjs, and I focus on building dynamic, scalable web applications</p>
       <div className="hero-action">
         <div className="hero-connect"><AnchorLink className="anchor-link" offset={70} href="#contact" >Connect With Me</AnchorLink></div>
         <div className="hero-resume">My Resume</div>
